14408c86a9c96d09d2806b494d2a28857c391629
Sunil <36020672+sudo-sunil@users.noreply.github.com>
2021-03-22 16:42:33 +0530

    Subrtraction ~ Sunil

--- Calcyclone/Program.cs
+++ Calcyclone/Program.cs
@@ -10,6 +10,17 @@ namespace Calcyclone
     {
         static void Main(string[] args)
         {
+            Program p = new Program();
+            p.sub();
+            Console.ReadLine();
+        }
+        
+        public void sub()
+        {
+            int x = 20;
+            int y = 4;
+            int result = x - y;
+            Console.WriteLine("Subtraction is : {0}", result);
         }
     }
 }
