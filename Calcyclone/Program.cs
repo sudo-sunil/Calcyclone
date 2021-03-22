46b4a3ed2729c5126fd24bec1ea46bea59bca1d3
Sunetra9999 <sunetraseshagiri98@gmail.com>
2021-03-22 16:43:43 +0530

    Sunetra has added div function.

--- Calcyclone/Program.cs
+++ Calcyclone/Program.cs
@@ -8,8 +8,20 @@ namespace Calcyclone
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            void div()
+            {
+                Console.WriteLine("Enter 1st no:");
+                int x = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter 2nd no:");
+                int y = int.Parse(Console.ReadLine());
+
+                int result = x / y;
+                Console.WriteLine("Div: " + result);
+
+            }
+               
         }
     }
 }
