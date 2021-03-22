22f6d30a81d943a7d69aafcc74b6ce06abf63283
shubhamkhadki <shubhamkhadki909@gmail.com>
2021-03-22 16:57:47 +0530

    addition draft_2

--- Calcyclone/Program.cs
+++ Calcyclone/Program.cs
@@ -12,6 +12,7 @@ namespace Calcyclone
         {
             Program p = new Program();
             p.sub();
+            p.add(45.5f, 65.7f);
             Console.ReadLine();
         }
         
@@ -21,6 +22,11 @@ namespace Calcyclone
             int y = 4;
             int result = x - y;
             Console.WriteLine("Subtraction is : {0}", result);
+        }
+
+        public void add(float x, float y)
+        {
+            Console.WriteLine("Addition is : " + (x + y));
         }
     }
 }
